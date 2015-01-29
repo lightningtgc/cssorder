d6e3871971897ee77fb6467fcdba23e2d0f94924
lightningtgc <lightningtgc@gmail.com>
2015-01-29 16:24:32 +0800

    Improve main

--- cssorder.js
+++ cssorder.js
@@ -1,4 +1,8 @@
 ﻿var CSSComb = require('csscomb');
+var fs      = require('fs');
+var path    = require('path');
+
+var PATH_CONFIG = '/config/config.json';
 
 /**
  * preHandleSrc
@@ -8,43 +12,43 @@
  * @return string
  */
 function preHandleSrc(cssSrc) {
-
   /* 
-   * fix like: .a{ filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e6529dda', endColorstr='#e6529dda', GradientType=0)\9;}
+   * Fix like: .a{ filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e6529dda', endColorstr='#e6529dda', GradientType=0)\9;}
    * which has two semicolon( : ) will cause parse error.
    */
-  cssSrc = cssSrc.replace(/progid(\s)?:(\s)?/g, '#tidy1#');
+  cssSrc = cssSrc.replace(/progid(\s)?:(\s)?/g, '#order1#');
 
   /*
-   * fix absolute url path like: 
+   * Fix absolute url path like: 
    * .a { background: url("http://www.qq.com/one.png");} 
    *
    */
-  cssSrc = cssSrc.replace(/:\/\//g, '#tidy2#');
+  cssSrc = cssSrc.replace(/:\/\//g, '#order2#');
 
   /*
-   * fix base64 url like: 
+   * Fix base64 url like: 
    * .a { background: url("data:image/png;base64,iVBOMVEX///////////////+g0jAqu8zdII=");} 
    *
    */
   cssSrc = cssSrc.replace(/data[\s\S]+?\)/g, function(match){
-      match = match.replace(/:/g, '#tidy3#');
-      match = match.replace(/;/g, '#tidy4#');
-      match = match.replace(/\//g, '#tidy5#');
+      match = match.replace(/:/g, '#order3#');
+      match = match.replace(/;/g, '#order4#');
+      match = match.replace(/\//g, '#order5#');
       return match;
   });
+
   /*
-   * fix multiple line comment include single comment //
+   * Fix multiple line comment include single comment //
    * eg: / * sth // another * /
    */
   cssSrc = cssSrc.replace(/\/\*[\s\S]+?\*\//g, function(match){
       // handle single comment //
-      match = match.replace(/\/\//g, '#tidy6#');
+      match = match.replace(/\/\//g, '#order6#');
       return  match;
   });
 
   /*
-   * fix single comment like:  // something 
+   * Fix single comment like:  // something 
    * It can't works in IE, and will cause parse error
    * update: handle single line for compressive case
    */
@@ -54,9 +58,9 @@ function preHandleSrc(cssSrc) {
       if (match.indexOf('{') === -1 || match.indexOf('}') === -1 || match.indexOf(':') === -1) {
           var targetMatch;
 
-          //handle first line
+          //Handle first line
           if (match.charAt(0) !== '/' ) {
-              // remove first string and / and \ 
+              // Remove first string and / and \ 
               targetMatch = match.substr(1).replace(/\\|\//g, '');
               return match.charAt(0) + '/*' + targetMatch + '*/';
           } else {
@@ -79,14 +83,14 @@ function preHandleSrc(cssSrc) {
  */
 function afterHandleSrc (content) {
 
-    content = content.replace(/#tidy1#/g, 'progid:');
-    content = content.replace(/#tidy2#/g, '://');
-    content = content.replace(/#tidy3#/g, ':');
-    content = content.replace(/#tidy4#/g, ';');
-    content = content.replace(/#tidy5#/g, '/');
-    content = content.replace(/#tidy6#/g, '//');
+    content = content.replace(/#order1#/g, 'progid:');
+    content = content.replace(/#order2#/g, '://');
+    content = content.replace(/#order3#/g, ':');
+    content = content.replace(/#order4#/g, ';');
+    content = content.replace(/#order5#/g, '/');
+    content = content.replace(/#order6#/g, '//');
 
-    // handle compressive css file 
+    // Handle compressive css file 
     content = content.replace(/(\}|\*\/)(?!\r|\n).*/g, function(match){
         // Deal with multiple line comment include block {}
         if (match.indexOf('*/') > 0 && match.indexOf('/*') === -1) {
@@ -100,25 +104,51 @@ function afterHandleSrc (content) {
 }
 
 
+/**
+ * CSSOrder
+ * Main Fun
+ *
+ * @param {string || object} config
+ * @return {undefined}
+ */
 function CSSOrder = function(config) {
-    var cssComb = new CSSComb();
+    var path = path.join(process.cwd(), PATH_CONFIG);
 
+    // Set custom configure translate to object
+    if (!config) {
+        try {
+            config = JSON.parse(fs.readFileSync(path, 'utf8'));
+        } catch(e) {
+            throw new Error('CSSOrder:Parse default config error.');
+        }
+    }
+
+    var cssComb = new CSSComb(config);
+
+    /**
+     * processText
+     * Export a text handle method.
+     *
+     * @param {string}text
+     * @param {object} options
+     * @return {string}
+     */
     cssComb.processText = function(text, options){
-        var content;
+        var content = '';
         text = preHandleSrc(text);
         content = cssComb.processString(text, options);
         content = afterHandleSrc(content);
 
-        // remove ^M and fix newLine issue in windows
+        // Remove ^M and fix newLine issue in windows
         if (process.platform === 'win32') {
             content = content.replace(/\r/g, '');
             /* content = content.replace(/\n/g, '\r\n'); */
         }
+
         return content;
     }
 
     return cssComb;
-
 }
 
 module.exports = CSSOrder;
